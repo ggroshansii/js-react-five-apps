05b7196e3e4f24ba26ba5ada154d1a6f7fa7b8a1
ggroshansii <garthgroshansii@gmail.com>
2021-09-29 13:21:24 -0400

    added spacing styles and added onChange() events that update the corresponding state when each input field is changed

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,35 @@
+import "./ContactForm.css";
+import { useState } from "react";
+
 
 
 function ContactForm() {
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [address, setAddress] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+
+    console.log(firstName, lastName, address, phoneNumber);
+
     return (
-        <div>I am the contact form!</div>
+        <>
+            <div className="form-container">
+                <h1>Contact Form</h1>
+                <form action="">
+                <label htmlFor="first-name">First Name:</label>
+                    <input id="first-name" type="text" name="first-name" placeholder="Enter First Name" value={firstName} onChange={(event)=> setFirstName(event.target.value)} />
+                    <label htmlFor="last-name">Last Name:</label>
+                    <input id="last-name" type="text" name="last-name" placeholder="Enter Last Name" value={lastName} onChange={(event)=> setLastName(event.target.value)}/>
+                    <label htmlFor="address">Address: </label>
+                    <input id="address" type="text" name="address" placeholder="Enter Address" value={address} onChange={(event)=> setAddress(event.target.value)}/>
+                    <label htmlFor="phone-number">Phone Number: </label>
+                    <input id="phone-number" type="text" name="phone-number" placeholder="Enter Phone Number" value={phoneNumber} onChange={(event)=> setPhoneNumber(event.target.value)}/>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </>
     )
 }
+
 export default ContactForm;
