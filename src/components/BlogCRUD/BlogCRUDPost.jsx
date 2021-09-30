ba8f3c126c9014f1b46fefc8e92b739c08ccd975
ggroshansii <garthgroshansii@gmail.com>
2021-09-29 21:29:48 -0400

    styled blog post appearance and added two static buttons: Delete and Edit

--- src/components/BlogCRUD/BlogCRUDPost.jsx
+++ src/components/BlogCRUD/BlogCRUDPost.jsx
@@ -1,11 +1,17 @@
 
+import "./BlogCRUDPost.css";
+
 function BlogCRUDPost(props) {
     return (
-        <div>
+        <div className="blog-post-container">
+            <div className="blog-post-buttons">
+                <button>Delete</button>
+                <button>Edit</button>
+            </div>
             <h2>{props.title}</h2>
             <h3>{props.author}</h3>
             <p>{props.text}</p>
-            <p>{props.tags}</p>
+            <p>#{props.tags}</p>
         </div>
     )
 }
